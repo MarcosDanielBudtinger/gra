c2ecd68b6b35b2caa895b3463026ae749a902cc1
Marcos Budtinger <marcos.mdbbr@gmail.com>
2022-11-11 11:55:22 -0300

    remove public from MovieResourceTest

--- src/test/java/com/marcos/gra/resource/MovieResourceTest.java
+++ src/test/java/com/marcos/gra/resource/MovieResourceTest.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
-public class MovieResourceTest {
+class MovieResourceTest {
 
    private MockMvc mockMvc;
 
